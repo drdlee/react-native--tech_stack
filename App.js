595772af3e7340987f0ec548d4e8c079af82081c
drdlee <danielrudi.lee@gmail.com>
2018-03-01 18:55:51 +0800

    initializing the Reducers

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/component/common';
 
 class App extends Component {
   render() {
     return (
       <Provider store={createStore}>
-        <View />
+        <View>
+          <Header headerText="Tech Stack" />
+        </View>
       </Provider>
     );
   }
